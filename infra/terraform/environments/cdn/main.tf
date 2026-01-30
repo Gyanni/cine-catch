76a350fb67dec37fa5b8af6c25686880954af95f
Gyanni <lghlee36@gmail.com>
2026-01-30 15:37:44 +0900

    add IAM policy

--- infra/terraform/environments/cdn/main.tf
+++ infra/terraform/environments/cdn/main.tf
@@ -17,3 +17,72 @@ module "cdn" {
   domain_name  = var.domain_name
   environment  = var.environment
 }
+
+# GitHub Actions IAM 사용자 참조
+data "aws_iam_user" "github_actions" {
+  user_name = "github-actions"
+}
+
+# CDN 관련 IAM 정책
+resource "aws_iam_user_policy" "github_actions_cdn" {
+  name = "github-actions-cdn-policy"
+  user = data.aws_iam_user.github_actions.user_name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "ACMPermissions"
+        Effect = "Allow"
+        Action = [
+          "acm:RequestCertificate",
+          "acm:DescribeCertificate",
+          "acm:DeleteCertificate",
+          "acm:ListCertificates",
+          "acm:ListTagsForCertificate",
+          "acm:AddTagsToCertificate"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "CloudFrontPermissions"
+        Effect = "Allow"
+        Action = [
+          "cloudfront:CreateOriginAccessControl",
+          "cloudfront:GetOriginAccessControl",
+          "cloudfront:UpdateOriginAccessControl",
+          "cloudfront:DeleteOriginAccessControl",
+          "cloudfront:CreateDistribution",
+          "cloudfront:GetDistribution",
+          "cloudfront:UpdateDistribution",
+          "cloudfront:DeleteDistribution",
+          "cloudfront:TagResource",
+          "cloudfront:ListTagsForResource"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "S3BucketPermissions"
+        Effect = "Allow"
+        Action = [
+          "s3:CreateBucket",
+          "s3:DeleteBucket",
+          "s3:GetBucketVersioning",
+          "s3:PutBucketVersioning",
+          "s3:GetBucketPublicAccessBlock",
+          "s3:PutBucketPublicAccessBlock",
+          "s3:GetBucketPolicy",
+          "s3:PutBucketPolicy",
+          "s3:DeleteBucketPolicy",
+          "s3:ListBucket",
+          "s3:GetBucketTagging",
+          "s3:PutBucketTagging",
+          "s3:GetBucketLocation"
+        ]
+        Resource = [
+          "arn:aws:s3:::${var.project_name}-*"
+        ]
+      }
+    ]
+  })
+}
